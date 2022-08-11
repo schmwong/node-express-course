bd153fa61b09e31e766c88ba99e3b879220fa13f
schmwong <smwong87@gmail.com>
2022-08-11 13:52:03 +0800

    set up express server on port 8000

---
+++ server.js
@@ -0,0 +1,21 @@
+/*
+Before beginning, install express locally by running in this folder:
+npm init -y
+npm install express --save
+*/
+
+// gives you access to the express library by searching your node_modules for "express"
+const express = require("express");
+
+// creates an instance of the express constructor, which we will name "app"
+const app = express();
+
+// starts up the server locally on the port given as its first argument (8000)
+app.listen(8000, function () {
+  console.log("server is running");
+});
+
+/*
+Test run in terminal: node server.js
+ctrl C to quit
+*/
